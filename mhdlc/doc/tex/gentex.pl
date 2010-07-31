dcd93c3302550b0229482b6f872767dba71735df
zinces <zinces@cd89b550-3133-0410-a459-13e4e5e64c7f>
2010-07-31 07:10:34 +0000

    + Add BNF.tex generation
    git-svn-id: https://metahdl.googlecode.com/svn/trunk@30 cd89b550-3133-0410-a459-13e4e5e64c7f

---
+++ mhdlc/doc/tex/gentex.pl
@@ -0,0 +1,51 @@
+#! /usr/bin/perl
+
+my $file = shift;
+
+open FH, $file or die "ERROR:Can not open $file for read.$!\n";
+
+open TEX, "> BNF.tex" or die "ERROR:Can not open BNF.tex for write.$!\n";
+
+my $state = "other";
+
+while ( <FH> ) {
+
+    if ( $state eq "other" ) {
+	if ( /^Grammar$/ ) {
+	    $state = "grammar";
+	}
+    }
+    elsif ( $state eq "grammar" ) {
+	if ( /^Terminals, with rules where they appear$/ ) {
+	    exit;
+	}
+	elsif ( /^ +0 \$accept/ || /^ +\d+ \@/ || /^$/) { 
+	    # drop this line
+	}
+	else {
+	    $_ =~ s/^ +\d+ (.*)/$1/;
+	    $_ =~ s/\@\d //g;
+	    
+
+	    $_ =~ s/(\$|%|&|\{|\}|\#)/\\textbf{\\textcolor{red}{\\$1}}/g;
+	    $_ =~ s/([A-Z_]{2,})/\\textbf{\\textcolor{red}{$1}}/g;
+	    $_ =~ s/_/\\_/g;
+	    $_ =~ s/(\+|-|>|<)/\$$1\$/g;
+	    $_ =~ s/"(\^|~)"/\\textbf{\\textcolor{red}{\\$1\{\}}}/g;
+	    $_ =~ s/"(\S+)"/\\textbf{\\textcolor{red}{$1}}/g;
+	    $_ =~ s/\|/\$|\$/g; 
+
+	    if (  /^(\S+):/ ) {
+		$_ =~ s/^(\S+):/\n\\vspace{1em}\n\\noindent\n\\settowidth{\\parindent}{\\hspace{4ex}}\n$1 \$::=\$\\hspace{1ex}/;
+	    }
+	    else {
+		$_ =~ s/^\s+(.*)/\n\\mbox{$1}/;
+	    }
+	    print TEX;
+	}
+    }
+}
+close FH; 
+close TEX;
+
+    
